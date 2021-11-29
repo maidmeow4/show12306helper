56ccef0834503d163ee74e511483eeb4969e161c
喵喵喵喵四 <xul1994@outlook.com>
2021-11-29 12:00:08 +0800

    release version 1.1
    Updated debug message, include license.

--- script.user.js
+++ script.user.js
@@ -1,9 +1,10 @@
     // ==UserScript==
     // @name         显示被隐藏的 12306 订票帮手
     // @namespace    ShowMe12306TicketHelper
-    // @version      1.0
+    // @version      1.1
     // @description  显示被隐藏的12306订票帮手
     // @author       maidmeow4
+    // @license GPL-3.0-or-later
     // @match        *://kyfw.12306.cn/otn/leftTicket/*
     // @grant        none
     // ==/UserScript==
@@ -11,4 +12,4 @@
     var drawer = document.getElementById ("sear-sel-bd");
     var drawerHeight = 179;
     drawer.style.height = drawerHeight + 'px';
-    console.log('maidmeow4: Enlarged drawer to ' + drawerHeight + 'px!');
+    console.log('DEBUG: Enlarged drawer to ' + drawerHeight + 'px!');
