e5365df24922495e6ca2a4f5081590dd7fe9b8b4
喵喵喵喵四 <xul1994@outlook.com>
2022-02-19 23:01:39 +0800

    适应12306新式隐藏手段

--- script.user.js
+++ script.user.js
@@ -1,15 +1,20 @@
-    // ==UserScript==
-    // @name         显示被隐藏的 12306 订票帮手
-    // @namespace    ShowMe12306TicketHelper
-    // @version      1.1
-    // @description  显示被隐藏的12306订票帮手
-    // @author       maidmeow4
-    // @license GPL-3.0-or-later
-    // @match        *://kyfw.12306.cn/otn/leftTicket/*
-    // @grant        none
-    // ==/UserScript==
-     
-    var drawer = document.getElementById ("sear-sel-bd");
-    var drawerHeight = 179;
-    drawer.style.height = drawerHeight + 'px';
-    console.log('DEBUG: Enlarged drawer to ' + drawerHeight + 'px!');
+// ==UserScript==
+// @name         显示被隐藏的 12306 订票帮手
+// @namespace    ShowMe12306TicketHelper
+// @version      1.2
+// @description  显示被隐藏的12306订票帮手
+// @author       maidmeow4
+// @license GPL-3.0-or-later
+// @match        *://kyfw.12306.cn/otn/leftTicket/*
+// @grant        GM_addStyle
+// ==/UserScript==
+
+var drawer = document.getElementById("sear-sel-bd");
+var drawerHeight = 179;
+drawer.style.height = drawerHeight + 'px';
+console.log('Helper: Enlarged drawer to ' + drawerHeight + 'px!');
+
+let collection = document.getElementsByClassName("section clearfix");
+for (let i = 0; i < collection.length; i++) {
+    collection[i].style.display = "";
+}
